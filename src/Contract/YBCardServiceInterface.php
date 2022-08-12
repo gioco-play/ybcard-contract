9ffb7d3feffb6343744c55a6b03648efaa86b41e
q90016200 <q90016200@gmail.com>
2022-08-12 16:48:44 +0800

    支持帳號轉換

--- src/Contract/YBCardServiceInterface.php
+++ src/Contract/YBCardServiceInterface.php
@@ -81,5 +81,23 @@ interface YBCardServiceInterface {
      * @return mixed
      */
     function orderFailCheck(string $op_code, string $order_no);
+
+    /**
+     * 營商帳號轉換為遊戲商帳號
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $player_name
+     * @return mixed
+     */
+    function accountToVendor(string $op_code, array $vendor, string $player_name);
+
+    /**
+     * 遊戲商帳號轉換為營商帳號
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $vendor_account
+     * @return mixed
+     */
+    function accountToOperator(string $op_code, array $vendor, string $vendor_account);
 }
 
